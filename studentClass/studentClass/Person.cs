11cebb309c435aa139cf139a58f3f249459dc0da
rickyMio <ricoricky-26@hotmail.it>
2017-01-14 16:53:20 +0100

    Function Describe

--- studentClass/studentClass/Person.cs
+++ studentClass/studentClass/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,14 @@ namespace studentClass
             _age     = age;
         }
         
-        public string 
-    y}
+        public string describe()
+        {
+            string output = "NOME:       " + name    + "\r\n";
+            output       += "COGNOME:    " + surname + "\r\n";
+            output       += "ETA':       " + age     + "\r\n";
+            output       += "C. FISCALE: " + cf;
+
+            return output;
+        }
+    }
 }
